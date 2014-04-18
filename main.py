6464625ed80f58aa318f694536d69b22febf3293
Greg Sabo <gsabo@hubspot.com>
2014-04-17 22:50:26 -0400

    More variety in statements

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def random_identifier():
 ri = random_identifier
 
 def random_literal():
-    choice = random.randint(0, 3)
+    choice = random.randint(0, 2)
     if choice is 0:
         return str(random.randint(0, 100))
     if choice is 1:
@@ -28,27 +28,45 @@ def random_literal():
     if choice is 2:
         return random.choice(["True", "False"])
 
+def random_value(depth=1):
+    rv = random_value
+    choices = (0, 1, 2, 3, 4, 5)
+    if depth >= 3:
+        choices = (0, 1)
+
+    choice = random.choice(choices)
+    if choice is 0:
+        return random_literal()
+    if choice is 1:
+        return "%s()" % random_identifier()
+    if choice is 2:
+        return "(%s and %s)" % (rv(depth+1), rv(depth+1))
+    if choice is 3:
+        return "(%s or %s)" % (rv(depth+1), rv(depth+1))
+    if choice is 4:
+        return "%s(%s)" % (random_identifier(), rv(depth+1))
+    if choice is 5:
+        return "%s(%s, %s=%s)" % (random_identifier(), rv(depth+1), random_identifier(), rv(depth+1))
+
 def random_statement(indent=1):
     pad = "    " * indent
 
-    choice = random.randint(0, 1)
+    choice = random.randint(0, 2)
     if choice is 0 or indent >= 3:
-        return "%s%s = %s()" % (pad, random_identifier(), random_identifier())
+        return "%s%s = %s" % (pad, random_identifier(), random_value())
+
     if choice is 1:
         return """%sif %s:
 %s
 %selse:
-%s
-""" % (
-        pad, ri(),
+%s""" % (
+        pad, random_identifier(),
         random_statement(indent+1),
         pad,
         random_statement(indent+1))
+
     if choice is 2:
-        return """
-%s%s()
-""" % (pad, ri())
-    return "Whoops"
+        return "%s%s()" % (pad, random_identifier())
 
 class Cursor(object):
     def __init__(self):
@@ -64,11 +82,12 @@ class FakeFunc(object):
 
     def render(self):
         args = ", ".join(random_identifier() for i in range(self.num_args))
-        return """
-def %s(%s):
+        body = "\n".join(random_statement() for i in range(random.randint(0, 5)))
+        return """def %s(%s):
 %s
     return %s
-""" % (self.name, args, random_statement(), random_literal())
+
+""" % (self.name, args, body, random_value())
 
 def random_program():
     scope = []
