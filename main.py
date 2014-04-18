2ff4470156b61ccc690f2d365b1dee341e8d6a45
Greg Sabo <gsabo@hubspot.com>
2014-04-17 21:07:31 -0400

    Has branching logic now.

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import py_compile
 import random
 import string
 
+with open("/usr/share/dict/words", "r") as f:
+    WORDS = f.readlines()
+
+with open("/usr/share/dict/connectives", "r") as f:
+    CONNECTIVES = f.readlines()
+
 def random_identifier():
-    def gen():
-        for i in range(random.randint(3, 15)):
-            yield random.choice(string.lowercase)
-    return ''.join(gen())
+    if random.random() > 0.5:
+        return random.choice(WORDS).strip().lower()
+
+    i = random.choice(CONNECTIVES).strip()
+    i = i + "_"
+    i = i + random.choice(WORDS).strip()
+    return i.lower()
+
+ri = random_identifier
 
 def random_literal():
     choice = random.randint(0, 3)
@@ -15,10 +26,38 @@ def random_literal():
     if choice is 1:
         return "\"%s\"" % random_identifier()
     if choice is 2:
-        return random.choice("True", "False")
+        return random.choice(["True", "False"])
+
+def random_statement(indent=1):
+    pad = "    " * indent
+
+    choice = random.randint(0, 1)
+    if choice is 0 or indent >= 3:
+        return "%s%s = %s()" % (pad, random_identifier(), random_identifier())
+    if choice is 1:
+        return """%sif %s:
+%s
+%selse:
+%s
+""" % (
+        pad, ri(),
+        random_statement(indent+1),
+        pad,
+        random_statement(indent+1))
+    if choice is 2:
+        return """
+%s%s()
+""" % (pad, ri())
+    return "Whoops"
+
+class Cursor(object):
+    def __init__(self):
+        self.indent_levels = 0
+
 
 class FakeFunc(object):
-    def __init__(self):
+    def __init__(self, scope):
+        self.scope = scope
         self.num_args = random.randint(0, 3)
         self.name = random_identifier()
         self.uses = []
@@ -27,12 +66,15 @@ class FakeFunc(object):
         args = ", ".join(random_identifier() for i in range(self.num_args))
         return """
 def %s(%s):
-    %s()
+%s
     return %s
-""" % (self.name, args, random_identifier(), random_literal())
+""" % (self.name, args, random_statement(), random_literal())
 
 def random_program():
-    return FakeFunc().render()
+    scope = []
+    for i in range(100):
+        scope.append(FakeFunc(scope))
+    return "\n".join(f.render() for f in scope)
 
 def save_random_program():
     with open("output.py", "w") as f:
